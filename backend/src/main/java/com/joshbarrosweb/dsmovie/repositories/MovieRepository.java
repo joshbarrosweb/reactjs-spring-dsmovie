18973b8dabcb2e47f23363c1e9e6c4ff137098c5
Josue Barros <joshbarrosweb@gmail.com>
2022-03-10 21:21:50 -0300

    chore: find movies

---
+++ backend/src/main/java/com/joshbarrosweb/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.joshbarrosweb.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joshbarrosweb.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
