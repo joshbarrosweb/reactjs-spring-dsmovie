88a4017a4e94dfabdc1db4cb180e46947224d281
Josue Barros <joshbarrosweb@gmail.com>
2022-03-11 14:44:00 -0300

    chore: added pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,9 +29,13 @@ function Listing() {
         });
     }, [page]);
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
             <div className="container">
                 <div className="row">
